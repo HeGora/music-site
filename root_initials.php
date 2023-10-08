deac1522cf1818f09a5bedb2d69f7ab7a4602ada
HeGora <nikitos.gora@gmail.com>
2023-10-09 00:54:56 +0300

    not react files were deleted

--- root_initials.php
+++
@@ -1,9 +0,0 @@
-<?php
-$rootDir = "http://".$_SERVER['SERVER_NAME']."/my_site/";
-$musicDir = $rootDir."material/music/";
-$cssDir = $rootDir."css/";
-$jsDir = $rootDir."js/";
-$dbName = 'mysite_db_v2';
-
-
-?>
